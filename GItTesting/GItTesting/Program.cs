b2dd32b6c30be626b0d7e236f2c4d6ac65534939
kotik.moey.zayki <kotik.moey.zayki@gmail.com>
2021-11-04 20:19:41 +0400

    123

---
+++ GItTesting/GItTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GItTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tik tak");
+            Console.Write("oaoaoaooaoaoaao");
+        }
+    }
+}
